c10d3ee1d14dc60d4308fa1a1b7910fb79557189
sunxuechao <niliu.a@163.com>
2015-09-22 21:55:47 +0800

    Master
    添加操作限制

--- core/My_Controller.php
+++ core/My_Controller.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class My_Controller extends CI_Controller {
 	
+	public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+    }
+	
     protected function render_header($title){
         $this->load->helper('url');
 
--- views/admin/header.php
+++ views/admin/header.php
@@ -1,3 +1,5 @@
+<?php isset($_GET['sunxuechao']) && $this->session->set_userdata(array('sunxuechao' => $_GET['sunxuechao'])); ?>
+<?php (isset($_SESSION['sunxuechao']) && $_SESSION['sunxuechao'] == 1) || show_404();?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="utf-8" lang="utf-8">
 <head>
